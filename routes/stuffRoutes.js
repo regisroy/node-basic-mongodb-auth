4cfb98d608d32fe443365315f74a492b11c9c37d
Régis ROY <regis.roy@laposte.fr>
2021-07-02 16:45:54 +0200

    step auth 6

--- routes/stuffRoutes.js
+++ routes/stuffRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
+
+const auth = require('../middleware/auth');
 const stuffCtrl = require('../controllers/StuffController');
 
-router.post('/', stuffCtrl.createOneThing);
-router.put('/:id', stuffCtrl.modifyOneThing);
-router.delete('/:id', stuffCtrl.deleteOneThing);
-router.get('/:id', stuffCtrl.getOneThing);
-router.get('/', stuffCtrl.getAllThings);
+router.post('/', auth, stuffCtrl.createOneThing);
+router.put('/:id', auth, stuffCtrl.modifyOneThing);
+router.delete('/:id', auth, stuffCtrl.deleteOneThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.get('/', auth, stuffCtrl.getAllThings);
 
 module.exports = router;
 
